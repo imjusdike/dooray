2702f855b0e1504f55d39ba6c6a361936ddeafd5
imjusdike <iparkinggwangju@naver.com>
2025-04-16 18:01:17 +0900

    Update send_dooray_alert.py

--- send_dooray_alert.py
+++ send_dooray_alert.py
@@ -1,6 +1,6 @@
 import requests
 
-WEBHOOK_URL = "https://hook.dooray.com/services/xxx/yyy"  # 여기에 네 Webhook URL 넣기
+WEBHOOK_URL = "https://parkingcloud.dooray.com/services/2256444361245483660/4047412744015864813/Y8Oo9blQTZiMJPUEgl_0TQ"  # 여기에 네 Webhook URL 넣기
 
 payload = {
     "botName": "확도 알림 봇",
